227a80458b66ec2bf55e3d17c077686e6b2427ed
Antiloope <rodrigopizarro1234@gmail.com>
2020-06-07 15:11:05 -0300

    Component header (responsive)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +20,7 @@ import {MatButtonModule} from "@angular/material/button";
     BrowserAnimationsModule,
     MatGridListModule,
     MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
