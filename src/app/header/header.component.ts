d652043e1af84e6069c643075501a25d12d9cb57
Antiloope <rodrigopizarro1234@gmail.com>
2020-06-13 23:34:29 -0300

    Corrected fade on header image

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,7 +16,7 @@ export class HeaderComponent implements OnInit {
     const verticalOffset = window.pageYOffset
       || document.documentElement.scrollTop
       || document.body.scrollTop || 0;
-      if(verticalOffset <= 10){
+      if(verticalOffset <= 5){
         (document.querySelector('#image') as HTMLElement).style.backgroundImage =
           'linear-gradient(' +
           '          rgba(16, 16, 16, 0.0) 0%,' +
@@ -30,11 +30,11 @@ export class HeaderComponent implements OnInit {
         (document.querySelector('#image') as HTMLElement).style.backgroundImage =
           'linear-gradient(' +
           '      rgba(16, 16, 16, 0.0) 0%,' +
-          '      rgba(16, 16, 16, '+(0.8 - 2/verticalOffset)+') '+(60-(verticalOffset-20)/2)+'%,' +
-          '      rgba(16, 16, 16, '+(0.9 - 1/verticalOffset)+') 70%,' +
-          '      rgba(16, 16, 16, '+(0.95 - 1/verticalOffset)+') 80%,' +
-          '      rgba(16, 16, 16, '+(1 - 1/verticalOffset)+') 95%,' +
-          '      rgba(16, 16, 16, 1.0) 100%' +
+          '      rgba(16, 16, 16, 0.0) '+(60-verticalOffset/4)+'%,' +
+          '      rgba(16, 16, 16, 0.2) '+(80-verticalOffset/5)+'%,' +
+          '      rgba(16, 16, 16, 0.7) '+(90-verticalOffset/6)+'%,' +
+          '      rgba(16, 16, 16, 0.9) '+(95-verticalOffset/7)+'%,' +
+          '      rgba(16, 16, 16, 1.0) '+(100-verticalOffset/8)+'%' +
           '  )';
       }
   }
