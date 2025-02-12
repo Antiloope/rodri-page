3531472c115e7930e012e7b8fd158ca4855baedf
Antiloope <rodrigopizarro1234@gmail.com>
2025-02-11 21:26:22 -0300

    Upgrade versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
 import { TopFiveComponent } from './top-five/top-five.component';
 import { ContactComponent } from './contact/contact.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { AboutComponent } from './about/about.component';
-import { OwlModule } from 'ngx-owl-carousel';
 import { PlaylistsComponent } from './playlists/playlists.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SongComponent } from './song/song.component';
 import { AlbumViewComponent } from './album-view/album-view.component';
 import { PlaylistViewComponent } from './playlist-view/playlist-view.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'song/:id', component: SongComponent },
   { path: 'album/:id', component: AlbumViewComponent },
   { path: 'playlist/:id', component: PlaylistViewComponent },
@@ -42,10 +43,10 @@ const routes: Routes = [
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    OwlModule,
     RouterModule.forRoot(routes),
+    CarouselModule
   ],
-  exports: [RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
